fcd71cb3cfa70bc44ff8d359bb954ec9d3a2c796
Rajbir Sehmi <raj.sehmi1@yahoo.com>
2026-04-09 22:09:37 -0700

    Updated the app version to v1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.creative.shoppinglist"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

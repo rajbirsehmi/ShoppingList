c0583ca3d2340f336353f7774284f8954334cf08
Rajbir Sehmi <raj.sehmi1@yahoo.com>
2026-05-01 22:10:17 -0700

    Removed the tests from :app module

--- app/src/main/java/com/creative/shoppinglist/ui/screens/ShoppingScreenEmpty.kt
+++ app/src/main/java/com/creative/shoppinglist/ui/screens/ShoppingScreenEmpty.kt
@@ -21,7 +21,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 import androidx.compose.ui.platform.testTag
-import com.creative.shoppinglist.ui.components.TestTags
+import com.creative.shoppinglist.core.testing.TestTags
 
 @Composable
 fun ShoppingScreenEmpty(

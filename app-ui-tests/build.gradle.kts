718288bbdbb63ba36930bf9a55460625e3c389fd
Rajbir Sehmi <raj.sehmi1@yahoo.com>
2026-05-01 23:49:52 -0700

    Modified :app-ui-tests gradle

--- app-ui-tests/build.gradle.kts
+++ app-ui-tests/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -14,11 +15,23 @@ android {
     defaultConfig {
         minSdk = 33
         targetSdk = 35
-        testInstrumentationRunner = "com.creative.shoppinglist.HiltTestRunner"
+        testInstrumentationRunner = "com.creative.shoppinglist.core.testing.HiltTestRunner"
     }
 
     buildFeatures {
         compose = true
+    }
+
+    testOptions {
+        managedDevices {
+            localDevices {
+                create("device1") {
+                    device = "Pixel 2"
+                    apiLevel = 33
+                    systemImageSource = "aosp-atd"
+                }
+            }
+        }
     }
 }
 
@@ -31,9 +44,15 @@ dependencies {
     implementation(libs.hilt.android.testing)
     ksp(libs.hilt.android.compiler)
     
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui.test.junit4)
     
     implementation(libs.androidx.junit)
     implementation(libs.androidx.espresso.core)
     implementation(libs.androidx.rules)
+    
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+
+    implementation(libs.kotlinx.serialization.json)
 }
